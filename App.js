5beeb9fd77a0273ba936cf3fb8558878b3b79b32
rudrachouhan <rudra678chouhan@gmail.com>
2023-07-11 12:26:21 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+const CalculatorApp = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleAddition = () => {
+    const sum = Number(num1) + Number(num2);
+    setResult(`Sum: ${sum}`);
+  };
+
+  const handleSubtraction = () => {
+    const difference = Number(num1) - Number(num2);
+    setResult(`Difference: ${difference}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Calculator</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 1"
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={setNum1}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 2"
+        keyboardType="numeric"
+        value={num2}
+        onChangeText={setNum2}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleAddition}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleSubtraction}>
+        <Text style={styles.buttonText}>Subtract</Text>
+      </TouchableOpacity>
+      <Text style={styles.result}>{result}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    width: '80%',
+    height: 40,
+    backgroundColor: '#3498db',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  result: {
+    fontSize: 18,
+    marginTop: 20,
   },
 });
+
+export default CalculatorApp;
+
+
+
